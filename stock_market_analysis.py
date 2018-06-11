5f40d11f3e2feb2e4dd3ce3d99e0e5ba34ac6b54
TeYuanLiu <teyuanliu@ucla.edu>
2018-06-10 17:06:14 -0700

    preprocessing completed

---
+++ stock_market_analysis.py
@@ -0,0 +1,95 @@
+#######################
+# Author: Te-Yuan Liu
+#######################
+
+#######################
+# Import Library
+#######################
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#######################
+# Define Function
+#######################
+def preprocess():
+    print("start preprocessing...")
+    p_dic = {}
+    q_dic = {}
+    r_dic = {}
+    edgelist_dic = {}
+    file_count = 0
+    valid_count = 0
+    path = "./finance_data/data/"
+    for file in os.listdir(path):
+        if file.endswith(".csv"):
+            file_count += 1
+            file_name = str(file).strip(".csv")
+            df = pd.read_csv(path + str(file), header=0)
+            p_list = df["Close"].tolist()
+            if len(p_list) == 765:
+                valid_count += 1
+                p_dic[file_name] = p_list
+    for s, pl in p_dic.items():
+        q_list = []
+        r_list = []
+        for i in range(1,len(pl)):
+            q = (pl[i] - pl[i-1])/pl[i-1]
+            q_list.append(q)
+            r_list.append(np.log(1 + q))
+        q_dic[s] = q_list
+        r_dic[s] = r_list
+        if len(r_list) != 764:
+            print("return list length error...")
+            return
+    for s1, rl1 in r_dic.items():
+        for s2, rl2 in r_dic.items():
+            if s1 != s2:
+                key1 = s1 + "\n" + s2
+                key2 = s2 + "\n" + s1
+                if edgelist_dic.get(key1) == None and edgelist_dic.get(key2) == None:
+                    ri_avg = sum(rl1)/len(rl1)
+                    rj_avg = sum(rl2)/len(rl2)
+                    product_list = [a*b for a,b in zip(rl1,rl2)]
+                    ri_rj_avg = sum(product_list)/len(product_list)
+                    ri_sq_list = [a**2 for a in rl1]
+                    ri_sq_avg = sum(ri_sq_list)/len(ri_sq_list)
+                    rj_sq_list = [a**2 for a in rl2]
+                    rj_sq_avg = sum(rj_sq_list)/len(rj_sq_list)
+                    pij = (ri_rj_avg - ri_avg*rj_avg)/np.sqrt((ri_sq_avg - ri_avg**2)*(rj_sq_avg - rj_avg**2))
+                    if pij > 1 or pij < -1:
+                        print("pij value error...")
+                        return
+                    wij = np.sqrt(2*(1 - pij))
+                    edgelist_dic[key1] = wij
+                elif edgelist_dic.get(key1) != None and edgelist_dic.get(key2) != None:
+                    print("duplicate entry error...")
+                    return
+    ## create edge list file
+    """
+    with open("stock_network_edgelist.txt", "w") as outfile:
+        for k, v in edgelist_dic.items():
+            s1, s2 = k.split("\n")
+            line = ",".join([s1, s2, str(v)]) + "\n"
+            outfile.write(line)
+    """
+    ## plot histogram
+    w_list = []
+    for k, v in edgelist_dic.items():
+        w_list.append(v)
+    plt.hist(w_list, bins="auto")
+    plt.title("Histogram")
+    plt.show()
+    print(file_count)
+    print(valid_count)
+    print(len(p_dic))
+    print("preprocessing completed...")
+
+#######################
+# Main Function
+#######################
+def main():
+    preprocess()
+
+if __name__ == "__main__":
+    main()
---
+++ stock_network.r
@@ -0,0 +1,32 @@
+#######################
+# Author: Te-Yuan Liu
+#######################
+
+#######################
+# Import Library
+#######################
+library("igraph")
+library(data.table)
+
+#######################
+# Define Function
+#######################
+plot_dd = function(g){
+    d = degree(g, mode="all")
+    dd = degree.distribution(g, mode="all", cumulative=F)
+    degree = 1:max(d)
+    probability = dd[-1]
+    nonzero.position = which(probability != 0)
+    probability = probability[nonzero.position]
+    degree = degree[nonzero.position]
+    plot(probability ~ degree, xlab="Degree", ylab="Probability", col=1, main="Degree Distribution")
+}
+#######################
+# Main Function
+#######################
+main = function(){
+    dt = fread("stock_network_edgelist.txt", header=F, sep=",", fill=T)
+    g = graph.data.frame(dt, directed=F)
+    plot_dd(g)
+}
+main()
